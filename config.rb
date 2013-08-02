187a2f6317c2d79868b012d0746b16d813a9a539
Niki Liu <nishiki.liu@gmail.com>
2013-08-01 22:11:53 -0400

    Initial commit

---
+++ config.rb
@@ -0,0 +1,15 @@
+#------------------------------------------------------------------------
+# Configuration
+#------------------------------------------------------------------------
+
+activate :livereload
+activate :directory_indexes
+
+set :css_dir,    'stylesheets'
+set :js_dir,     'javascripts'
+set :images_dir, 'images'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
