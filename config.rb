a61d4f89b47d0defca516c7a5150f08c2a7f7c1d
Niki Liu <nishiki.liu@gmail.com>
2014-12-18 16:31:21 -0800

    Update gems and config

--- config.rb
+++ config.rb
@@ -1,47 +1,45 @@
-#------------------------------------------------------------------------
+#===============================================================================
 # config.rb
-#------------------------------------------------------------------------
+#===============================================================================
 
-# LiveReload and pretty URLs
+# Activate extensions
+#-------------------------------------------------------------------------------
 activate :livereload
+activate :autoprefixer
+
+# Paths
+#-------------------------------------------------------------------------------
 activate :directory_indexes
-
-# Use relative paths
 activate :relative_assets
-set :relative_links, true
+set      :relative_links, true
+set      :css_dir,        "stylesheets"
+set      :js_dir,         "javascripts"
+set      :images_dir,     "images"
 
-# Default layout
+# Layouts
+#-------------------------------------------------------------------------------
 page "/*", layout: "application"
 
-# Asset paths
-set :css_dir,    "stylesheets"
-set :js_dir,     "javascripts"
-set :images_dir, "images"
-
-# Haml config
-set :haml, { :attr_wrapper => '"' }
-
-# Build config
+# Assets
+#-------------------------------------------------------------------------------
+set :haml, attr_wrapper: '"'
 configure :build do
   activate :minify_css
   activate :minify_javascript
 end
 
-# Custom helpers
+# Helpers
+#-------------------------------------------------------------------------------
 helpers do
 
   # Given a default_title string, return a string in the format
   # "title | default_title", where title is the string defined by frontmatter.
   #
-  # @param  {default_title} - String
-  # @return {string}        - Formatted title
+  # @param  {String} - default_title
+  # @return {String}
   def page_title(default_title)
     title = current_page.data.title
-    if title
-      "#{title} | #{default_title}"
-    else
-      default_title
-    end
+    if title then "#{title} | #{default_title}" else default_title end
   end
 
 end
